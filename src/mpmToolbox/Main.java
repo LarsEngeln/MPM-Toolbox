54f5904a1a8cc03b90ff96c4d35b01893bb5d67f
Lars Engeln <mail@lars-engeln.de>
2026-09-22 10:09:29 +0200

    fixes axis and performance of Graph

--- src/mpmToolbox/Main.java
+++ src/mpmToolbox/Main.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  * @author Axel Berndt
  */
 public class Main {
-    public static final String version = "0.1.43 (alpha) - dateLine";
+    public static final String version = "0.1.43 (alpha) - volCurve";
 
     public static void main(String[] args) {
         // read the application settings from file
